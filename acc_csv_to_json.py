172927de56b76a1df1d40d8ea0e0cce67d619318
mikeev261 <mikeev@gmail.com>
2021-04-17 15:36:21 -0400

    Revamp of design. Increased reporting. Reports team num conflicts.

--- acc_csv_to_json.py
+++ acc_csv_to_json.py
@@ -5,29 +5,48 @@ import json
 import array
 import csv
 import code
+import yaml
 #USAGE: -i <inputfile.csv> -o <outputfile.json>
+
+
+yaml_path = "./CONFIG.yaml"
+
+try:
+    print("Importing " + yaml_path)
+    stream_yaml = open(yaml_path, "r")
+except Exception as e:
+    print(e)
+    exit()
+
+print("CONFIG.yaml Settings:")
+settings = yaml.full_load(stream_yaml)
+for node, node_settings in settings.items():
+    print(node + ": " + str(node_settings) + "\n") 
+
+f_errors = open("ERROR.log", "+w")
+ERROR_BUF = ""
+NUM_ERRORS = 0
+
+#code.interact(local=locals()) #Jump into interactive mode
 
 #################
 # ADJUSTMENTS
-TOTAL_GT3 = 50
-TOTAL_GT4 = 0
-TOTAL_CAR_PER_TYPE = 3
-
-
+TOTAL_GT3 = settings['car_limits']['total_gt3']
+TOTAL_GT4 = settings['car_limits']['total_gt4']
+TOTAL_CAR_PER_TYPE = settings['car_limits']['total_car_per_type']
 #################
 
 
-TEAM = 1
-RACE_NUM = 2
-FIRST = 3
-LAST = 4
-DISCORD_ID = 5
-STEAM_ID = 6
-LEAD_DRIVER = 7
-CAR_1 = 8
-CAR_2 = 9
-CAR_3 = 10
-
+TEAM = settings['google_form_alignment']['team']
+RACE_NUM = settings['google_form_alignment']['race_num']
+FIRST = settings['google_form_alignment']['first']
+LAST = settings['google_form_alignment']['last']
+DISCORD_ID = settings['google_form_alignment']['discord_id']
+STEAM_ID = settings['google_form_alignment']['steam_id']
+LEAD_DRIVER = settings['google_form_alignment']['lead_driver']
+CAR_1 = settings['google_form_alignment']['car_1']
+CAR_2 = settings['google_form_alignment']['car_2']
+CAR_3 = settings['google_form_alignment']['car_3']
 
 #Preload total allocations
 gt3_rem = TOTAL_GT3
@@ -40,92 +59,31 @@ print ("-----------------------------------")
 
 print ("\n AVAILABLE CARS:\n")
 
-car_dict = {
-    0: "Porsche 991 GT3 R",
-    1: "Mercedes AMG GT3 (2015)",
-    2: "Ferrari 488 GT3 (2018)",
-    3: "Audi R8 LMS GT3",
-    4: "Lamborghini Huracan GT3 (2015)",
-    5: "McLaren 650S GT3",
-    6: "Nissan GT-R GT3 (2018)",
-    7: "BMW M6 GT3",
-    8: "Bentley Continental GT3 (2018)",
-    9: "Porsche 991II GT3 Cup",
-    10: "Nissan GT-R GT3 (2015)",
-    11: "Bentley Continental GT3 (2015)",
-    12: "Aston Martin V12 Vantage GT3",
-    13: "Lamborghini Gallardo R-EX GT3",
-    14: "Jaaaaaaaaaaaaaaaaaaaaaaaaag GT3",
-    15: "Lexus RC F GT3",
-    16: "Lamborghini Huracan GT3 Evo (2019)",
-    17: "Honda NSX GT3 (2017)",
-    18: "Lamborghini Huracan SuperTrofeo",
-    19: "Audi R8 LMS GT3 Evo",
-    20: "Aston Martin V8 Vantage GT3",
-    21: "Honda NSX GT3 Evo (2019)",
-    22: "McLaren 720S GT3",
-    23: "Porsche 991 II GT3 R",
-    24: "Ferrari GT3 Evo (2020)",
-    25: "Mercedes AMG GT3 Evo (2020)",
-    26: "",
-    27: "",
-    28: "",
-    29: "",
-    30: "",
-    31: "",
-    32: "",
-    33: "",
-    34: "",
-    35: "",
-    36: "",
-    37: "",
-    38: "",
-    39: "",
-    41: "",
-    42: "",
-    43: "",
-    44: "",
-    45: "",
-    46: "",
-    47: "",
-    48: "",
-    49: "",
-    50: "Alpine A110 GT4",
-    51: "Aston Martin V8 Vantage GT4",
-    52: "Audi R8 LMS GT4",
-    53: "BMW M4 GT4",
-    54: "",
-    55: "Chevrolet Camaro GT4.R",
-    56: "Ginetta G55 GT4",
-    57: "KTM X-Bow GT4",
-    58: "Maserati Granturismo MC GT4",
-    59: "McLaren 570S GT4",
-    60: "Mercedes AMG GT4",
-    61: "Porsche 718 Cayman GT4 Clubsport",
-}
-
+car_dict = settings['cars']
 
 car_rem = array.array('I') #Instantiate car_rem array (empty)
+team_nums = array.array('I') #Instantiate team_nums array (empty)
 
+#Init car_rem array
 for car in car_dict:
     car_rem.append(TOTAL_CAR_PER_TYPE) #Initialize a new per-type value
     if(bool(car_dict[car])):
         print(car, ': ', car_dict[car])
 
-
 input_csv = ''
 output_json = 'entrylist.json'
 arguments = len(sys.argv) - 1
 if(arguments < 1):
-  print ('USAGE: acc_csv_to_json.py <inputfile.csv> <admin STEAM_ID (optional)>')
-  sys.exit(2)  
+    print ('USAGE: acc_csv_to_json.py <inputfile.csv> <admin STEAM_ID (optional)>')
+    sys.exit(2)  
 input_csv = sys.argv[1]
-try:
-    admin_steam_id = sys.argv[2]
-    print ('\nAdmin declared as ' + admin_steam_id)
-except:
-    admin_steam_id = ""
-    print ('\nINFO: No Admin ID Declared. If you want someone to have admin access, re-run with their S# as a 2nd argument.\n')
+
+#try:
+#    admin_steam_id = sys.argv[2]
+#    print ('\nAdmin declared as ' + admin_steam_id)
+#except:
+#    admin_steam_id = ""
+#    print ('\nINFO: No Admin ID Declared. If you want someone to have admin access, re-run with their S# as a 2nd argument.\n')
 
 print ('\nINFO: Input CSV: '+input_csv+'\n')
 print ('INFO: Output JSON: entrylist.json\n')
@@ -156,6 +114,24 @@ def check_car(driver, car, num):
             else: 
                 return True
 
+def error(message):
+    global NUM_ERRORS
+    global ERROR_BUF
+    print(message)
+    print("\n")
+    NUM_ERRORS += 1
+    ERROR_BUF = ERROR_BUF + "\n" + message
+
+
+#Checks driver numbers to make sure there are no repeats. If it detects a repeat, throw an error
+def check_nums(driver, team, num):
+    other_team_lead = ""
+    if(int(num) in team_nums):
+        error("ERROR: the team number " + num + " lead by " + driver + " conflicts with existing team lead by " + other_team_lead)
+        #exit()
+    else:
+        team_nums.append(int(num))
+
 def select_car(driver, car1, car2, car3):
     if(check_car(driver, car1, 1)):
         return car1
@@ -170,7 +146,22 @@ def select_car(driver, car1, car2, car3):
         print("ERROR: CAR3 = ", car3)
         sys.exit()
 
-
+def list_selected_cars():
+    print("\n\n\nCARS SELECTED:")
+    index = 0
+    for car in car_rem:
+        if(car_dict[index]):
+            cars = TOTAL_CAR_PER_TYPE - car_rem[index]
+            if(cars != 0):
+                print(car_dict[index] + " : " + str(cars))
+        index += 1
+def list_exhausted_cars():
+    print("\n\n\nEXHAUSTED CARS: ")
+    index = 0
+    for car in car_rem:
+        if(car_rem[index] == 0): 
+            print(car_dict[index])
+        index += 1
 
 
 def read_csv_write_json(input_csv, lead):
@@ -200,14 +191,16 @@ def read_csv_write_json(input_csv, lead):
                 car_num1 = car_num_lookup(car_dict, row[CAR_1])
                 car_num2 = car_num_lookup(car_dict, row[CAR_2])
                 car_num3 = car_num_lookup(car_dict, row[CAR_3])
-                drivername = str(row[FIRST] + row[LAST])
+                drivername = str(row[FIRST] + " " + row[LAST])
                 car_num = select_car(drivername, car_num1, car_num2, car_num3)
+                car_rem[car_num] -= 1
                 gt3 = car_num < 50
                 if(gt3):
                     gt3_rem -= 1
                 else: 
                     gt4_rem -= 1
                 team = {}
+                team_name = row[TEAM]
 
             else: #not lead
                 if(row[LEAD_DRIVER] == "Yes"): #Lead driver, skip for this pass (we got them already)
@@ -232,7 +225,7 @@ def read_csv_write_json(input_csv, lead):
                 team['drivers'] = [driver]
                 team['overrideDriverInfo'] = 1
             
-                if(driver['playerID']==admin_steam_id):
+                if(driver['playerID'] in settings['admins']):
                     team['isServerAdmin'] = 1
                 else:
                     team['isServerAdmin'] = 0
@@ -242,6 +235,7 @@ def read_csv_write_json(input_csv, lead):
                 team['raceNumber'] = int(race_num)
                 entries.append(team)
                 num_lead += 1
+                check_nums(drivername, team_name, race_num)
     
             if(not lead): #We are now iterating through the dict to add the teammates
                 #num_team = 0
@@ -291,5 +285,16 @@ final_gt3 = int(TOTAL_GT3) - int(gt3_rem)
 final_gt4 = TOTAL_GT4 - gt4_rem
 print ("GT3 Cars: ", final_gt3)
 print ("GT4 Cars: ", final_gt4)
+
+list_selected_cars()
+list_exhausted_cars()
+
 print ('DONE!')
-#Close the JSON outfile
+
+if(NUM_ERRORS):
+    print("ERRORS: " + str(NUM_ERRORS) + " errors saved into ERROR.log")
+    errors = f_errors.readlines()
+    print(ERROR_BUF)
+    f_errors.write(ERROR_BUF)
+
+f_errors.close()
